c0edfbb4808c77370e919473d6a519b6b68a21db
gonzales54 <gonzalesitokawa@gmail.com>
2022-09-11 09:30:08 +0900

    running reverse position stop

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,4 +1,8 @@
-const app = document.getElementById('app');
+let count = 0;
+let intervalId: any;
+let key = '' as string;
+
+const app: HTMLElement|null = document.getElementById('app');
 const S_mario = [
     [0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0],
     [0,0,0,0,2,2,2,2,2,2,2,2,2,0,0,0],
@@ -57,7 +61,6 @@ const R_B_mario = [
 ] as number[][]
 
 const R_N_mario = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0],
     [0,0,0,0,2,2,2,2,2,2,2,2,2,0,0,0],
     [0,0,0,0,3,3,3,1,1,3,1,0,0,0,0,0],
@@ -74,6 +77,7 @@ const R_N_mario = [
     [0,0,0,0,0,2,2,2,3,3,3,0,0,0,0,0],
     [0,0,0,0,0,3,3,3,3,3,3,3,0,0,0,0],
     [0,0,0,0,0,3,3,3,3,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ] as number[][]
 
 const R_A_mario = [
@@ -96,14 +100,15 @@ const R_A_mario = [
     [],
 ] as number[][]
 
-const RunningBeforeMario = (mario: number[][]):void => {
-    const elem = document.createElement('div');
-    elem.setAttribute('id', 'mario');
+const Mario = (mario: number[][], key: string):void => {
+    (document.getElementById(`${key}`) as HTMLElement|null)?.remove();
+    const elem: HTMLDivElement = document.createElement('div');
+    elem.setAttribute('id', `${key}`);
     app?.appendChild(elem);
     for(let i = 0; i < mario.length; i++) {
         for(let j = 0; j < mario[i].length; j++) {
-            const elem = document.createElement('p');
-            const body = document.getElementById("mario");
+            const elem: HTMLParagraphElement = document.createElement('p');
+            const body: HTMLElement|null = document.getElementById(`${key as string}`);
             switch(mario[i][j] as number) {
                 case 0:
                     elem.classList.add('disable-block');
@@ -129,156 +134,58 @@ const RunningBeforeMario = (mario: number[][]):void => {
     } 
 }
 
-const RunningNowMario = (mario: number[][]):void => {
-    const elem = document.createElement('div');
-    elem.setAttribute('id', 'mario');
-    app?.appendChild(elem);
-    for(let i = 0; i < mario.length; i++) {
-        for(let j = 0; j < mario[i].length; j++) {
-            const elem = document.createElement('p');
-            const body = document.getElementById("mario");
-            switch(mario[i][j] as number) {
-                case 0:
-                    elem.classList.add('disable-block');
-                    body?.appendChild(elem);
-                break;
-    
-                case 1:
-                    elem.classList.add('skin-block');
-                    body?.appendChild(elem);
-                break;
-    
-                case 2:                
-                    elem.classList.add('red-block');
-                    body?.appendChild(elem);
-                break;
-    
-                default:               
-                    elem.classList.add('green-block');
-                    body?.appendChild(elem);
-                break;
-            }
+
+Mario(S_mario, 'mario')
+
+const run = (count: number, key: string) => {
+    intervalId = setInterval(function(): void {
+        switch(count) {
+            case 0:
+                Mario(R_B_mario, key);
+                count += 1;
+            break;
+
+            case 1:
+                Mario(R_N_mario, key);
+                count += 1;
+            break;
+
+            default:
+                Mario(R_A_mario, key);
+                count = 0;
+            break;
         }
-    } 
+    }, 100);
 }
 
-const RunningAfterMario = (mario: number[][]):void => {
-    const elem = document.createElement('div');
-    elem.setAttribute('id', 'mario');
-    app?.appendChild(elem);
-    for(let i = 0; i < mario.length; i++) {
-        for(let j = 0; j < mario[i].length; j++) {
-            const elem = document.createElement('p');
-            const body = document.getElementById("mario");
-            switch(mario[i][j] as number) {
-                case 0:
-                    elem.classList.add('disable-block');
-                    body?.appendChild(elem);
-                break;
-    
-                case 1:
-                    elem.classList.add('skin-block');
-                    body?.appendChild(elem);
-                break;
-    
-                case 2:                
-                    elem.classList.add('red-block');
-                    body?.appendChild(elem);
-                break;
-    
-                default:               
-                    elem.classList.add('green-block');
-                    body?.appendChild(elem);
-                break;
-            }
+const destroy = (key1: string, key2: string|null) => {
+    for(let i=0; i<(app as HTMLElement)?.childElementCount;i++) {
+        if((app as HTMLElement).children[i].id === null) {
+            (app as HTMLElement).children[i].remove();
         }
-    } 
-}
-
-const StandMario = (mario: number[][]): void => {    
-    const elem = document.createElement('div');
-    elem.setAttribute('id', 'mario');
-    app?.appendChild(elem);
-    for(let i = 0; i < mario.length; i++) {
-        for(let j = 0; j < mario[i].length; j++) {
-            const elem = document.createElement('p');
-            const body = document.getElementById("mario");
-            switch(mario[i][j] as number) {
-                case 0:
-                    elem.classList.add('disable-block');
-                    body?.appendChild(elem);
-                break;
-    
-                case 1:
-                    elem.classList.add('skin-block');
-                    body?.appendChild(elem);
-                break;
-    
-                case 2:                
-                    elem.classList.add('red-block');
-                    body?.appendChild(elem);
-                break;
-    
-                default:               
-                    elem.classList.add('green-block');
-                    body?.appendChild(elem);
-                break;
-            }
-        }
-    } 
-}
-const JumpMario = (mario: number[][]): void => {    
-    const elem = document.createElement('div');
-    elem.setAttribute('id', 'mario');
-    app?.appendChild(elem);
-    for(let i = 0; i < mario.length; i++) {
-        for(let j = 0; j < mario[i].length; j++) {
-            const elem = document.createElement('p');
-            const body = document.getElementById("mario");
-            switch(mario[i][j] as number) {
-                case 0:
-                    elem.classList.add('disable-block');
-                    body?.appendChild(elem);
-                break;
-    
-                case 1:
-                    elem.classList.add('skin-block');
-                    body?.appendChild(elem);
-                break;
-    
-                case 2:                
-                    elem.classList.add('red-block');
-                    body?.appendChild(elem);
-                break;
-    
-                default:               
-                    elem.classList.add('green-block');
-                    body?.appendChild(elem);
-                break;
-            }
-        }
-    } 
-}
-
-let i = 0;
-setInterval(function() {
-    switch(i) {
-        case 0:
-            document.getElementById('mario')?.remove();
-            RunningBeforeMario(R_B_mario);
-            i += 1;
-        break;
-
-        case 1:
-            document.getElementById('mario')?.remove();
-            RunningNowMario(R_N_mario);
-            i += 1;
-        break;
-
-        default:
-            document.getElementById('mario')?.remove();
-            RunningAfterMario(R_A_mario);
-            i = 0;
-        break;
     }
-}, 100)
+    (document.getElementById(`${key1}`) as HTMLElement|null)?.remove();
+    (document.getElementById(`${key2}`) as HTMLElement|null)?.remove();
+    clearInterval(intervalId)
+}
+
+
+function keyPressEvent(e: KeyboardEvent) {
+    if(e.key === 'ArrowRight') {
+        destroy('mario', 'mario_reverse')
+        key = 'mario'
+        run(count, key)
+    } else if(e.key === 'ArrowLeft') {
+        destroy('mario', 'mario_reverse')
+        key = 'mario_reverse'
+        run(count, key)
+    } else if(e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+        return false;
+    } else {
+        destroy(key, '');
+        Mario(S_mario, key)
+        key = ''
+    }
+}
+
+document.addEventListener('keydown', keyPressEvent);
